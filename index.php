5f9bd288bc8a6f2d678151f0dbe9810e5f7ded99
kitruitt82 <kitruitt82@gmail.com>
2019-10-10 13:15:00 -0400

    Changed home route

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $f3 = Base::instance();
 
 $f3->route('GET|POST /', function (){
     $view = new Template();
-    echo $view->render('views/home.html');
+    echo $view->render('views/registered-participants.html');
 });
 
 $f3->run();
