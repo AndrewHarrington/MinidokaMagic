60f26a1984651fe3a06cbf55ce7dff018ea06d63
kitruitt82 <kitruitt82@gmail.com>
2019-10-10 13:09:57 -0400

    Added basic routing

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+//error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once('vendor/autoload.php');
+//require('model/functions.php');
+
+//start the session
+session_start();
+
+$f3 = Base::instance();
+
+$f3->route('GET|POST /', function (){
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+$f3->run();
