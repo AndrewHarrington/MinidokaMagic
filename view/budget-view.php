78b5ce8e8b46e2415ba2611555b2754e6b120f7d
Andrew Harrington <aharrington4@mail.greenriver.edu>
2019-10-22 19:56:08 -0400

    Users can now click on and view any PDF via a PDF viewer

---
+++ view/budget-view.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>View Budget</title>
+</head>
+<body>
+    <embed src="../uploads/<?echo $_GET['name']?>" width= "500" height= "375">
+</body>
+</html>
