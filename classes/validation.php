b7b0b4026159378fadeaf1ab38e740eac3e4285b
ryguy806 <rguelzo@mail.greenriver.edu>
2019-10-22 13:53:52 -0400

    Started validation class and updated validation to the index.

---
+++ classes/validation.php
@@ -0,0 +1,56 @@
+<?php
+
+function validForm(){
+    global $f3;
+    $isValid = true;
+
+    if(!validName($f3->get('fname'))){
+        $isValid = false;
+        $f3->set("errors['fname']", "Not a valid first name, please reenter!");
+    }
+
+    if(!validName($f3->get('lname'))){
+        $isValid = false;
+        $f3->set("errors['lname']", "Not a valid last name, please reenter");
+    }
+
+    if(!validPhone($f3->get('phone'))){
+        $isValid = false;
+        $f3->set("errors['phone']", "Not a valid phone number, please reenter");
+    }
+
+    if(!validPhone($f3->get('ephone'))){
+        $isValid = false;
+        $f3->set("errors['ephone']", "Not a valid phone number, please reenter");
+    }
+
+    if (!validAge($f3->get('age'))) {
+        $isValid = false;
+        $f3->set("errors['age']", "Please enter 1 or more.");
+    }
+
+    function validEmail($email){
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+
+    return $isValid;
+
+}
+
+function validName($name){
+    return (!empty($name) && ctype_alpha($name));
+}
+
+function validAge($age){
+    return (!empty($age) && ctype_digit($age));
+}
+
+function validPhone($phone){
+    return (preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/", $phone)
+            || preg_match("/^[0-9]{10}/", $phone)) && (mb_strlen((string)$phone) == 10 ||
+            mb_strlen((string)$phone) == 13);
+}
+
+function validEmail($email){
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
--- index.php
+++ index.php
@@ -24,4 +24,61 @@ $f3->route('GET|POST /', function ($f3){
     echo $view->render('view/registered-participants.html');
 });
 
+
+//error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once('vendor/autoload.php');
+
+$f3 = Base::instance();
+global $db;
+
+$f3->route('GET|POST /data', function ($f3) {
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        $f3->reroute("/data");
+    }
+    $view = new Template();
+    echo $view->render('view/login.html');
+});
+
+$f3->route('GET|POST /', function ($f3) {
+    $db = new Database();
+    $data = $db->getRegistrationData();
+    $f3->set('registrations', $data);
+    $view = new Template();
+    echo $view->render('view/registered-participants.html');
+});
+
+$f3->route('GET|POST /new_user', function ($f3) {
+
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $phone = $_POST['phone'];
+        $ephone = $_POST['ephone'];
+        $age = $_POST['age'];
+        $survivor = $_POST['survivor'];
+        $attended = $_POST['prevattend'];
+
+        $f3->set('fname', $fname);
+        $f3->set('lname', lname);
+        $f3->set('phone', $phone);
+        $f3->set('ephone', $phone);
+        $f3->set('age', $age);
+        $f3->set('survivor', $survivor);
+        $f3->set('attended', $attended);
+
+        if (validFrom()) {
+            //TODO
+        }
+    }
+
+    $view = new Template();
+    echo $view->render('view/participant_form.html');
+});
+
+
+$f3->run();
+
 $f3->run();
