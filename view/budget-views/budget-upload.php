7cfe2cd52b4ab35614c9f2c9f06120749969a679
Andrew Harrington <aharrington4@mail.greenriver.edu>
2019-10-24 14:30:47 -0400

    Merged a bunch of individual parts together

--- view/budget-views/budget-upload.php
+++ view/budget-views/budget-upload.php
@@ -1,43 +1,5 @@
 <?php
-if($_SERVER['REQUEST_METHOD'] == "POST"){
-    $target_dir = "../../uploads/";
-    $target_file = $target_dir . basename($_FILES["file"]["name"]);
-    $uploadOk = 1;
-    $file_type=$_FILES['file']['type'];
 
-    //validate that the file is a pdf
-    if(isset($_POST["submit"])) {
-
-        if($file_type=="application/pdf") {
-            //echo "File is a PDF.";
-            $uploadOk = 1;
-        } else {
-            echo "File is not a PDF.";
-            $uploadOk = 0;
-        }
-    }
-
-    // Check if file already exists
-    if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
-        $uploadOk = 0;
-    }
-
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
-    // if everything is ok, try to upload file
-    } else {
-        //save the pdf to an "uploads" folder
-        if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
-            //reroute to a master page to select a pdf to view
-            header("Location: budget-master-view.html");
-            //echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
-        } else {
-            echo "Sorry, there was an error uploading your file.";
-        }
-    }
-}
 ?>
 <!--<!DOCTYPE html>-->
 <!--<html lang="en">-->
@@ -47,10 +9,20 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 <!--</head>-->
 <!--<body>-->
 <include href="view/header.html"/>
-    <form action="" method="post" enctype="multipart/form-data">
-        <label for="file">Select File:</label>
-        <input type="file" name="file" id="file">
-        <button type="submit" id="submit" name="submit">Upload File</button>
-    </form>
+    <div class="container">
+        <form action="" method="post" enctype="multipart/form-data">
+            <div class="input-group">
+                <button class="input-group-text" type="submit" id="submit" name="submit">Upload File</button>
+                <div class="custom-file">
+                    <input class="custom-file-input" type="file" name="file" id="file">
+                    <label class="custom-file-label" for="file">Select File:</label>
+                </div>
+
+            </div>
+        </form>
+    </div>
+<ul class="container" id="box"></ul>
+
+<script src="model/javascript/budget-grab.js"></script>
 </body>
 </html>
