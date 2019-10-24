c00cfe56eb31225a9cb9d46e173a4c5035a65ff3
Andrew Harrington <aharrington@mail.greenriver.edu>
2019-10-24 14:25:38 -0400

    Minor Bug Fix

--- index.php
+++ index.php
@@ -50,9 +50,57 @@ $f3->route('GET|POST /registrations', function ($f3) {
     echo $view->render('view/registered-participants.html');
 });
 
-$f3->route('GET /budget-pdf', function (){
+$f3->route('GET|POST /budget-pdf', function (){
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $target_dir = "uploads/";
+        $target_file = $target_dir . basename($_FILES["file"]["name"]);
+        $uploadOk = 1;
+        $file_type=$_FILES['file']['type'];
+
+        //validate that the file is a pdf
+        if(isset($_POST["submit"])) {
+
+            if($file_type=="application/pdf") {
+                //echo "File is a PDF.";
+                $uploadOk = 1;
+            } else {
+                echo "File is not a PDF.";
+                $uploadOk = 0;
+            }
+        }
+
+        //Check file name length < 23chars
+        if(strlen($_FILES["file"]["name"]) > 23){
+            echo "Sorry, file name too long. ";
+            $uploadOk = 0;
+        }
+
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            echo "Sorry, file already exists.";
+            $uploadOk = 0;
+        }
+
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            echo "Your file was not uploaded.";
+            // if everything is ok, try to upload file
+        } else {
+            //save the pdf to an "uploads" folder
+            if (!move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+                //reroute to a master page to select a pdf to view
+                echo "Sorry, there was an error uploading your file.";
+                //echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
+            }
+        }
+    }
     $view = new Template();
     echo $view->render('view/budget-views/budget-upload.php');
+});
+
+$f3->route('GET /budget-view/@fileName', function (){
+    $view = new Template();
+    echo $view->render('view/budget-views/budget-view.php');
 });
 
 $f3->route('GET|POST /new_user', function ($f3) {
@@ -69,7 +117,7 @@ $f3->route('GET|POST /new_user', function ($f3) {
         $f3->set('fname', $fname);
         $f3->set('lname', $lname);
         $f3->set('phone', $phone);
-        $f3->set('ephone', $phone);
+        $f3->set('ephone', $ephone);
         $f3->set('age', $age);
         $f3->set('survivor', $survivor);
         $f3->set('attended', $attended);
