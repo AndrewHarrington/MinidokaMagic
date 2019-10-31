2bb399e53826969023d05d83b297f038804a1d88
Andrew Harrington <aharrington4@mail.greenriver.edu>
2019-10-31 16:35:56 -0400

    Seperated some of the javascript

---
+++ model/javascript/budget-grab.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    var pdfFilesDirectory = 'uploads/';
+
+    // get auto-generated page
+    $.ajax({url: pdfFilesDirectory}).then(function(html) {
+        // create temporary DOM element
+        var document = $(html);
+        console.log($(html));
+        // find all links ending with .pdf
+        //[href$=.pdf]
+        document.find('a[href$=pdf]').each(function() {
+            var pdfName = $(this).text();
+
+
+            //ISSUE: PDF File names can not be longer than 20 characters
+
+
+            //write each element as a clickable image/text
+            $("#box").append("<li>" +
+                "<a href='budget-view/" + pdfName + "'>" +
+                "<img src='styles/images/pdf-icon.png' alt='pdf-icon' style='width: 30px; height: 30px;'>" +
+                pdfName +
+                "</a>" +
+                "</li>");
+        })
+    });
+});
