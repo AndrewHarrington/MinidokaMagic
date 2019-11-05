df291adceb163f706697d911047981f015a2eeeb
ryguy806 <rguelzo@mail.greenriver.edu>
2019-11-05 15:10:10 -0500

    Began adding front end validation to the participant form.

--- model/javascript/participant-form.js
+++ model/javascript/participant-form.js
@@ -8,3 +8,38 @@ $(document).ready(function(){
         }
     });
 });
+
+// $(document).ready(function(){
+//     $("#modal").click(function(){
+//         if($("#submit").is(":clicked")){
+//             $("#modal").attr("hidden", false);
+//         }
+//         else{
+//             $("#res").attr("hidden", true);
+//         }
+//     });
+// });
+
+function validateForm() {
+    var x = document.forms["participant"]["fname"].value;
+    if (x == "") {
+        alert("First name must be filled out.");
+        return false;
+    }
+    x = document.forms["participant"]["lname"].value;
+    if(x == ""){
+        alert("Last name must be filled out.");
+    }
+    x = document.forms["participant"]["email"].value;
+    if(x == ""){
+        alert("Last name must be filled out.");
+    }
+    x = document.forms["participant"]["phone"].value;
+    if(x == ""){
+        alert("Last name must be filled out.");
+    }
+    x = document.forms["participant"]["ephone"].value;
+    if(x == ""){
+        alert("Last name must be filled out.");
+    }
+}
