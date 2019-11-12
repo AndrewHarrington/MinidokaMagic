8f34258a6002ff483e794bf71bd278823f586f6a
Andrew Harrington <aharrington4@mail.greenriver.edu>
2019-11-12 14:07:05 -0500

    Table now updates based on session data

--- model/javascript/volunteers.js
+++ model/javascript/volunteers.js
@@ -1,39 +1,51 @@
 $(document).ready(function() {
-    $('#volunteers').DataTable( {
-        columnDefs: [
-            {
-                className: "dt-left",
-                targets: [0, 1, 2, 3, 4, 5]
-            }
-        ],
-        responsive: {
-            details: {
-                display: $.fn.dataTable.Responsive.display.modal( {
-                    header: function ( row ) {
-                        var data = row.data();
-                        return 'Details for '+data[0]+' '+data[1];
-                    }
-                } ),
-                renderer: $.fn.dataTable.Responsive.renderer.tableAll( {
-                    tableClass: 'table'
-                } )
-            }
-        },
-        "lengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]]
-    } );
+    let cols = document.getElementsByTagName("th").length;
+    if(cols == 5){
+        $('#volunteers').DataTable( {
+            columnDefs: [
+                {
+                    className: "dt-left",
+                    targets: [0, 1, 2, 3, 4]
+                }
+            ],
+            responsive: {
+                details: {
+                    display: $.fn.dataTable.Responsive.display.modal( {
+                        header: function ( row ) {
+                            var data = row.data();
+                            return 'Details for '+data[0]+' '+data[1];
+                        }
+                    } ),
+                    renderer: $.fn.dataTable.Responsive.renderer.tableAll( {
+                        tableClass: 'table'
+                    } )
+                }
+            },
+            "lengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]]
+        } );
+    }
+    else{
+        $('#volunteers').DataTable( {
+            columnDefs: [
+                {
+                    className: "dt-left",
+                    targets: [0, 1, 2, 3, 4, 5]
+                }
+            ],
+            responsive: {
+                details: {
+                    display: $.fn.dataTable.Responsive.display.modal( {
+                        header: function ( row ) {
+                            var data = row.data();
+                            return 'Details for '+data[0]+' '+data[1];
+                        }
+                    } ),
+                    renderer: $.fn.dataTable.Responsive.renderer.tableAll( {
+                        tableClass: 'table'
+                    } )
+                }
+            },
+            "lengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]]
+        } );
+    }
 } );
-// $(document).ready(function() {
-//     $('#registrants').DataTable(
-//     {
-//         columnDefs: [
-//             {
-//                 className: "dt-left",
-//                 targets: [0, 1, 2, 3, 4, 5]
-//             }
-//         ],
-//         scrollY:        '50vh',
-//         scrollX:        '50vh',
-//         paging:         '50vh'
-//     }
-//     );
-// } );
