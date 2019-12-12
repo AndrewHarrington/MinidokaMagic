3a4f144f0e9e8f4f5b66edcb53c2299102f4f468
Katherine Truitt <kitruitt82@mail.com>
2019-12-12 12:34:53 -0500

    Final Commit

---
+++ model/javascript/archive-table.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+    $('#registrants').DataTable ({
+
+        columnDefs: [
+            {
+                className: "dt-left",
+                targets: [0, 1, 2, 3, 4]
+            },
+            {
+                responsive: true
+            },
+            {
+                className: "dt-center",
+                targets: [5,6,7]
+            }],
+        responsive: {
+
+            details: {
+
+                display: $.fn.dataTable.Responsive.display.modal({
+                    header: function ( row ) {
+                        let data = row.data();
+                        return 'Details for '+data[0]+' '+ data[1];
+                    }
+                }),
+
+                renderer: $.fn.dataTable.Responsive.renderer.tableAll ({
+
+                    tableClass: 'table',
+
+                })
+            }
+        },
+        "lengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]]
+    } );
+} );
